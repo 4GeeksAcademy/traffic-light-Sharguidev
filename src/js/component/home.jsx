d883e7858c3250790885f8e84e76c89ddbd14c08
Guillermo Obando <guillermoj.obando@gmail.com>
2024-09-19 04:44:31 +0000

    Semaforo funcional

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
 //create your first component
 const Home = () => {
+
+	const [color, setColor] = useState("red");
+	
+	const luces = [{nombre: "rojo", valor: "red"}, {nombre: "amarillo", valor: "yellow"}, {nombre: "verde", valor: "green"}]
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container mt-5 bg-black rounded" style={{width : 135}}>
+			<div className="d-inline-block d-flex flex-column align-items-center">
+				<button className={"d-inline-block bg-danger rounded-circle m-3 " + (color == 'red' ? 'rojo' : '')} style={{height: 80, width:80}} onClick={()=>{
+					setColor("red");
+				}}/>
+				<button className={"d-inline-block bg-warning rounded-circle m-3 " + ( color == 'yellow' ? 'amarillo' : '')} style={{height: 80, width:80}} onClick={()=>{
+					setColor("yellow");
+				}}/>
+				<button className={"d-inline-block bg-success rounded-circle m-3 " + (color == 'green' ? 'verde' : '')} style={{height: 80, width:80}} onClick={()=>{
+					setColor("green");
+				}}/>
+			</div>
 		</div>
 	);
 };
